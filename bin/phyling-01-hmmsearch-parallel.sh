7ad7b385522fe5900f9e7257052bc2ff3856a709
Jason Stajich <jason.stajich@ucr.edu>
2017-11-12 16:59:23 -0800

    template for sbatch run

--- bin/phyling-01-hmmsearch-parallel.sh
+++ bin/phyling-01-hmmsearch-parallel.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-if [ $MODULESHOME != "" ]; then
+#SBATCH --nodes 1 --ntasks 4 --mem 2G --out=logs/hmmsearch.%A.log
+
+if [ $MODULESHOME ]; then
     module load hmmer/3
 fi
 PEPDIR=pep
+PEPEXT=aa.fasta
 HMM_FOLDER=HMM
 HMMSEARCH_CUTOFF=1e-10
 HMMSEARCH_OUT=search
 LOG_FOLDER=logs
+CPU=2
 if [ -f config.txt ]; then
  source config.txt
 else
@@ -19,14 +23,16 @@ if [ ! $HMM ]; then
  echo "need to a config file to set the HMM folder name"
 fi
 
-G=$1
-if [ ! $G ]; then
-    echo "NO INPUT GENOME FILE"
-    exit
+if [ ! $INPUTFILE ]; then
+    INPUTFILE=$1
+    if [ ! $INPUTFILE ]; then
+	echo "NO INPUT GENOME FILE"
+	exit
+    fi
 fi
-
-NM=`basename $G .aa.fasta`
-echo "g=$G"
+INPUTFILE=$(basename $INPUTFILE)
+NM=$(basename $INPUTFILE .$PEPEXT)
+echo "g=$INPUTFILE NM=$NM"
 
 MARKERS=${HMM_FOLDER}/$HMM/markers_3.hmmb
 OUT=$HMMSEARCH_OUT/$HMM
@@ -38,8 +44,9 @@ if [ $SLURM_CPUS_ON_NODE ]; then
 fi
 # convention is they all end in .aa.fasta - change this if not or make a variable
 
-if [[ ! -f "$OUT/$NM.domtbl" || $PEPDIR/$G -nt $OUT/$NM.domtbl ]]; then
- echo "hmmsearch --cpu $CPU -E $CUTOFF --domtblout $OUT/$NM.domtbl $MARKERS $PEPDIR/$G >& $OUT/$NM.log"
+if [[ ! -f "$OUT/$NM.domtbl" ||
+      $PEPDIR/$INPUTFILE -nt $OUT/$NM.domtbl ]]; then
+ hmmsearch --cpu $CPU -E $HMMSEARCH_CUTOFF --domtblout $OUT/$NM.domtbl $MARKERS $PEPDIR/$INPUTFILE >& $OUT/$NM.log
 else
  echo "skipping $NM - has already run"
 fi
