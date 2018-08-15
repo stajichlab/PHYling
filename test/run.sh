5e59472535222d9004373f1b9477b94452b31c03
Jason Stajich <jasonstajich.phd@gmail.com>
2018-08-15 14:24:20 -0700

    stand alone tests

---
+++ test/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+module load hmmer/3
+module load python/3
+
+if [ ! -d HMM ]; then
+	wget https://github.com/1KFG/Phylogenomics_HMMs/releases/download/v1.1/fungal_HMMs-1.0.zip
+	unzip fungal_HMMs-1.0.zip
+	mv Phylogenomics_HMMs-1.0/HMM .
+	unlink fungal_HMMs-1.0.zip
+	rm -rf Phylogenomics_HMMs-1.0
+fi
+
+../PHYling init
+../PHYling search
+../PHYling aln
