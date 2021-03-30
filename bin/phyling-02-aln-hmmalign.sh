689cee66889fe89ff51241c2fd6f1fd0fcfedffc
Jason Stajich <jasonstajich.phd@gmail.com>
2021-03-30 09:40:11 -0700

    support chunking PHYling aln runs when slurm arrayjobs are run; makefiles take HMM input so diff marker sets are used

--- bin/phyling-02-aln-hmmalign.sh
+++ bin/phyling-02-aln-hmmalign.sh
@@ -9,6 +9,7 @@
 #SBATCH --time=2:00:00
 #SBATCH --output=logs/hmmaln.%A_%a.out
 
+ALNCHUNK=1
 OUTPEPEXT=aa.fa
 RESOVERLAP=0.70
 SEQOVERLAP=80
@@ -16,61 +17,74 @@ TRIMALSCHEME=-automated1
 CONFIG="config.txt"
 
 if [[ -f "$CONFIG" ]]; then
-    source "$CONFIG"
+  source "$CONFIG"
 else
-    echo "Need a \"$CONFIG\" file"
-    exit
+  echo "Need a \"$CONFIG\" file"
+  exit
 fi
 
 if [[ "$MODULESHOME" ]]; then
-    module load hmmer/3
-    module load trimal
-    module load clipkit
-    # module load BMGE # is this too slow?
+  module load hmmer/3
+  module load trimal
+  module load clipkit
+  # module load BMGE # is this too slow?
 fi
 
 if [[ -z "$HMM_FOLDER" ]]; then
-    echo "Need HMM_FOLDER set"
-    exit
+  echo "Need HMM_FOLDER set"
+  exit
 fi
 
 if [[ ! -d "$HMM_FOLDER" ]]; then
-    echo "HMM_FOLDER \"$HMM_FOLDER\" does not exist"
-    exit
+  echo "HMM_FOLDER \"$HMM_FOLDER\" does not exist"
+  exit
 fi
 
 ALN_OUTDIR="aln"
 
-while getopts c:f:i: OPT; do
-    case $OPT in
-      f) FORCE=${OPTARG};;
-      i) IN=${OPTARG};;
-    esac
+while getopts c:f:i:s:H: OPT; do
+  case $OPT in
+    f) FORCE=${OPTARG} ;;
+    i) IN=${OPTARG} ;;
+    s) ALNCHUNK=${OPTARG} ;;
+    H) HMM=${OPTARG} ;;
+  esac
 done
 
 [[ -z "$ALNFILES" ]] && ALNFILES="alnlist.$HMM"
 
 if [[ ! -f "$ALNFILES" ]]; then
-    echo "ALNFILES \"$ALNFILES\" does not exist"
-    exit 1
+  echo "ALNFILES \"$ALNFILES\" does not exist"
+  exit 1
 fi
 
 if [[ -z "$HMM" ]]; then
-    echo "Need config file to set the HMM folder name"
-    exit 1
+  echo "Need config file to set the HMM folder name"
+  exit 1
 fi
 
 DIR="$ALN_OUTDIR/$HMM"
 DBDIR="$HMM_FOLDER/$HMM/HMM3"
 
+MARKERS=()
 if [[ ${SLURM_ARRAY_TASK_ID} ]]; then
-    IN=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$ALNFILES")
+  # start when we have chunk ranges to run
+  START=$(python -c "print( ((${SLURM_ARRAY_TASK_ID} - 1) * $ALNCHUNK) + 1)")
+  END  =$(python -c "print( ${SLURM_ARRAY_TASK_ID} * $ALNCHUNK )")
+  IN=$(sed -n "$START,$ENDp" "$ALNFILES")
+  sed -n "$START,$ENDp" "$ALNFILES" | while read NM
+  do
+    MARKERS+=( $(basename "$IN" ".$OUTPEPEXT" ) )
+  done
+else
+  MARKERS=( $(basename "$IN" ".$OUTPEPEXT") )
 fi
 
-MARKER="$(basename "$IN" ".$OUTPEPEXT")"
-echo "IN=$IN gene=$MARKER"
 
-make -f $PHYLING_DIR/util/makefiles/Makefile.hmmalign SEQOVERLAP=$SEQOVERLAP RESOVERLAP=$RESOVERLAP $DIR/$MARKER.aa.clipkit
-make -f $PHYLING_DIR/util/makefiles/Makefile.hmmalign SEQOVERLAP=$SEQOVERLAP RESOVERLAP=$RESOVERLAP $DIR/$MARKER.cds.clipkit
-
+for MARKER in "${MARKERS[@]}"
+do
+  echo "gene=$MARKER"
+  make -f $PHYLING_DIR/util/makefiles/Makefile.hmmalign SEQOVERLAP=$SEQOVERLAP RESOVERLAP=$RESOVERLAP HMM=$HMM $DIR/$MARKER.aa.clipkit
+  make -f $PHYLING_DIR/util/makefiles/Makefile.hmmalign SEQOVERLAP=$SEQOVERLAP RESOVERLAP=$RESOVERLAP HMM=$HMM $DIR/$MARKER.cds.clipkit
+done
 exit
--- bin/phyling-02-aln.sh
+++ bin/phyling-02-aln.sh
@@ -4,6 +4,7 @@
 QUEUEING=parallel
 JOBCPU=1
 TOTALCPU=4
+ALNCHUNK=1
 OUTPEPEXT=aa.fa
 ALN_OUTDIR=aln
 ALNTOOL=hmmalign
@@ -11,32 +12,33 @@ EXPECTED=expected_prefixes.lst
 CONFIG="config.txt"
 
 if [[ -f "$CONFIG" ]]; then
-    source "$CONFIG"
+  source "$CONFIG"
 else
-    echo "Need a \"$CONFIG\" file"
-    exit
+  echo "Need a \"$CONFIG\" file"
+  exit
 fi
 
 JOBCPU=1 # force this to 1 since all of these steps are single threaded
 
 if [[ -z "$HMM" ]]; then
-    echo "Need config file to set the HMM folder name"
-    exit 1
+  echo "Need config file to set the HMM folder name"
+  exit 1
 fi
 
 [[ -z "$ALNFILES" ]] && ALNFILES="alnlist.$HMM"
 
-while getopts t:c:f:q: OPT; do
-    case $OPT in
-         t) ALNTOOL=${OPTARG};;
-         c) CLEAN=${OPTARG};;
-         f) FORCE=${OPTARG};;
-         q) QUEUEING=${OPTARG};;
-    esac
+while getopts t:c:f:s:q: OPT; do
+  case $OPT in
+    t) ALNTOOL=${OPTARG} ;;
+    c) CLEAN=${OPTARG} ;;
+    f) FORCE=${OPTARG} ;;
+    s) ALNCHUNK=${OPTARG} ;;
+    q) QUEUEING=${OPTARG} ;;
+  esac
 done
 
 if [[ ! -f "$ALNFILES" || "$CLEAN" == "1" || "$FORCE" == "1" ]]; then
-    find "$ALN_OUTDIR/$HMM" -name "*.$OUTPEPEXT" > "$ALNFILES"
+  find "$ALN_OUTDIR/$HMM" -name "*.$OUTPEPEXT" > "$ALNFILES"
 fi
 
 SCRIPT_DIR=$(dirname "$0")
@@ -44,77 +46,79 @@ SUBJOB_SCRIPT="${SCRIPT_DIR}/phyling-02-aln-$ALNTOOL.sh"
 COMBINE_SCRIPT="${SCRIPT_DIR}/phyling-03-aln-combine.sh"
 
 if [[ $QUEUEING == "parallel" ]]; then
-    JOBPARALLEL=$((TOTALCPU / JOBCPU))
-    echo "Run parallel job $ALNTOOL"
-    #echo "$JOBPARALLEL $SUBJOB_SCRIPT"
-    parallel -j "$JOBPARALLEL" \
-        "$SUBJOB_SCRIPT" \
-        -f "$FORCE" \
-        -c "$CLEAN" \
-        -i {} < "$ALNFILES"
+  JOBPARALLEL=$((TOTALCPU / JOBCPU))
+  echo "Run parallel job $ALNTOOL"
+  #echo "$JOBPARALLEL $SUBJOB_SCRIPT"
+  parallel -j "$JOBPARALLEL" \
+    "$SUBJOB_SCRIPT" \
+    -f "$FORCE" \
+    -c "$CLEAN" \
+    -i {} < "$ALNFILES"
 
-#    echo  "$COMBINE_SCRIPT" -x "$EXPECTED" -e cdsaln.trim -t cds
+  #    echo  "$COMBINE_SCRIPT" -x "$EXPECTED" -e cdsaln.trim -t cds
 
-    if [[ $ALNTOOL == "muscle" ]]; then
-	"$COMBINE_SCRIPT" -x "$EXPECTED" -e aa.denovo.trim -t aa
-    	if [ -d $CDSDIR ]; then
-	    "$COMBINE_SCRIPT" -x "$EXPECTED" -e cds.denovo.trim -t cds
-	fi
-    else
-	"$COMBINE_SCRIPT" -x "$EXPECTED" -e "aa.clipkit" -t aa
-    	if [ -d $CDSDIR ]; then
-	    "$COMBINE_SCRIPT" -x "$EXPECTED" -e cdsaln -t cds
-	fi
+  if [[ $ALNTOOL == "muscle" ]]; then
+    "$COMBINE_SCRIPT" -x "$EXPECTED" -e aa.denovo.trim -t aa
+    if [ -d $CDSDIR ]; then
+      "$COMBINE_SCRIPT" -x "$EXPECTED" -e cds.denovo.trim -t cds
     fi
+  else
+    "$COMBINE_SCRIPT" -x "$EXPECTED" -e "aa.clipkit" -t aa
+    if [ -d $CDSDIR ]; then
+      "$COMBINE_SCRIPT" -x "$EXPECTED" -e "cds.clipkit" -t cds
+    fi
+  fi
 
 
 elif [[ $QUEUEING == "slurm" ]]; then
-    QUEUECMD=""
-    [[ -n $QUEUE ]] && QUEUECMD="-p $QUEUE"
+  QUEUECMD=""
+  [[ -n $QUEUE ]] && QUEUECMD="-p $QUEUE"
 
-    ALNCT=$(wc -l "$ALNFILES" | awk '{print $1}')
-    PHYLING_DIR=$(dirname "$0")
+  ALNCT=$(wc -l "$ALNFILES" | awk '{print $1}')
+  ALNCTDIV=$(expr $ALNCT / $ALNCHUNK)
+  PHYLING_DIR=$(dirname "$0")
+  #echo "DEBUG: ALNCT is $ALNCT ALNCTDIV is $ALNCTDIV and ALNFILES was $ALNFILES"
+  echo "DEBUG: PHYLING_DIR is $PHYLING_DIR"
+  ARRAY="1-$ALNCTDIV"
+  CMD="sbatch --ntasks $JOBCPU --nodes 1 $QUEUECMD --export=PHYLING_DIR=\"$PHYLING_DIR\" --export=FORCE=$FORCE --array=$ARRAY $SUBJOB_SCRIPT"
+  echo "DEBUG: $CMD"
+  SUBMIT_MSG=$(eval $CMD)
+  SUBMIT_ID=$(echo "$SUBMIT_MSG" | awk '{print $4}')
+  echo "SUBMIT_ID is $SUBMIT_ID; $SUBMIT_MSG"
 
-    echo "PHYLING_DIR is $PHYLING_DIR"
-    ARRAY="1-$ALNCT"
+  if [[ $ALNTOOL == "muscle" ]]; then
+    echo "DEBUG: eady to run with $COMBINE_SCRIPT no extra ext"
+    sbatch --depend=afterany:"$SUBMIT_ID" \
+      $QUEUECMD \
+      --export=EXT=aa.denovo.trim,TYPE=aa,EXPECTED=\"$EXPECTED\" \
+      $COMBINE_SCRIPT
 
-    CMD="sbatch --ntasks $JOBCPU --nodes 1 $QUEUECMD --export=PHYLING_DIR=\"$PHYLING_DIR\" --export=FORCE=$FORCE --array=$ARRAY $SUBJOB_SCRIPT"
-
-    SUBMIT_MSG=$(eval $CMD)
-    SUBMIT_ID=$(echo "$SUBMIT_MSG" | awk '{print $4}')
-    echo "SUBMIT_ID is $SUBMIT_ID; $SUBMIT_MSG"
-
-    if [[ $ALNTOOL == "muscle" ]]; then
-	echo "ready to run with $COMBINE_SCRIPT no extra ext"
-        sbatch --depend=afterany:"$SUBMIT_ID" \
-            $QUEUECMD \
-            --export=EXT=aa.denovo.trim,TYPE=aa,EXPECTED=\"$EXPECTED\" \
-            $COMBINE_SCRIPT
-
-	if [ -d $CDSDIR ]; then
-            sbatch --depend=afterany:"$SUBMIT_ID" \
-		$QUEUECMD \
-		--export=EXT=cds.denovo.trim,TYPE=CDS,EXPECTED=\"$EXPECTED\" \
-		"$COMBINE_SCRIPT"
-	fi
-    else
-	echo "ready to run with $COMBINE_SCRIPT no extra ext"
-        CMD="sbatch --depend=afterany:$SUBMIT_ID $QUEUECMD --export=EXT=aa.clipkit,TYPE=aa,EXPECTED=\"$EXPECTED\" $COMBINE_SCRIPT"
-	eval $CMD
-
-	if [ -d $CDSDIR ]; then
-            CMD="sbatch --depend=afterany:$SUBMIT_ID $QUEUECMD --export=EXT=cds.clipkit,EXPECTED=$EXPECTED,TYPE=CDS $COMBINE_SCRIPT"
-	    eval $CMD
-	fi
+    if [ -d $CDSDIR ]; then
+      sbatch --depend=afterany:"$SUBMIT_ID" \
+        $QUEUECMD \
+        --export=EXT=cds.denovo.trim,TYPE=CDS,EXPECTED=\"$EXPECTED\" \
+        "$COMBINE_SCRIPT"
     fi
+  else
+    echo "ready to run with $COMBINE_SCRIPT no extra ext"
+    CMD="sbatch --depend=afterany:$SUBMIT_ID $QUEUECMD --export=EXT=aa.clipkit,TYPE=aa,EXPECTED=\"$EXPECTED\" $COMBINE_SCRIPT"
+    eval $CMD
+
+    if [ -d $CDSDIR ]; then
+      CMD="sbatch --depend=afterany:$SUBMIT_ID $QUEUECMD --export=EXT=cds.clipkit,EXPECTED=$EXPECTED,TYPE=CDS $COMBINE_SCRIPT"
+      eval $CMD
+    fi
+  fi
 else
-    echo "Run in serial"
-    TMP=$(mktmp)
-    find "$ALN_OUTDIR/$HMM" -name "*.$OUTPEPEXT" > "$TMP"
-    while read -r FILE; do
-        $SUBJOB_SCRIPT -f "$FORCE" -i "$FILE"
-    done < "$TMP"
-    rm "$TMP"
-    # do combine here (how to avoid duplicate code with
-    # the parallel stuff above
+  echo "Run in serial"
+  TMP=$(mktmp)
+  find "$ALN_OUTDIR/$HMM" -name "*.$OUTPEPEXT" > "$TMP"
+  while read -r FILE; do
+    $SUBJOB_SCRIPT -f "$FORCE" -i "$FILE" -c "$CLEAN"
+  done < "$TMP"
+  rm "$TMP"
+  # do combine here (how to avoid duplicate code with
+  # the parallel stuff above
+  "$COMBINE_SCRIPT" -x "$EXPECTED" -e "cds.clipkit" -t cds
+  "$COMBINE_SCRIPT" -x "$EXPECTED" -e "aa.clipkit" -t aa
 fi
