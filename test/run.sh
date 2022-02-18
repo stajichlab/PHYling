711105965f8c8ba7986eaf7a1e0d37ddf22ec60a
Jason Stajich <jasonstajich.phd@gmail.com>
2022-02-17 21:29:34 -0800

    set the config

--- test/run.sh
+++ test/run.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 module load hmmer/3
-module load python/3
+module unload miniconda
+module load miniconda3
 
-if [ ! -d HMM ]; then
-	wget https://github.com/1KFG/Phylogenomics_HMMs/releases/download/v1.1/fungal_HMMs-1.0.zip
-	unzip fungal_HMMs-1.0.zip
-	mv Phylogenomics_HMMs-1.0/HMM .
-	unlink fungal_HMMs-1.0.zip
-	rm -rf Phylogenomics_HMMs-1.0
+DIR=HMM
+MARKER=fungi_odb10
+URL=https://busco-data.ezlab.org/v4/data/lineages/fungi_odb10.2020-09-10.tar.gz
+
+if [ ! -d $DIR/fungi_odb10 ]; then
+    mkdir -p $DIR/$MARKER
+    curl -C - -O $URL
+    FILE=$(basename $URL)
+    tar zxf $FILE
+    
+    mv $MARKER/scores_cutoff $MARKER/lengths_cutoff $DIR/$MARKER
+    mv $MARKER/hmms $DIR/$MARKER/HMM3
+    cat $DIR/$MARKER/HMM3/*.hmm > $DIR/$MARKER/markers_3.hmm
+    hmmconvert -b $DIR/$MARKER/markers_3.hmm > $DIR/$MARKER/markers_3.hmmb
+    
+    rm -rf fungi_odb10.2020-09-10.tar.gz $MARKER
 fi
 
 ../PHYling init
