3ca6156e9998d5f464f17d236f407e4228391d3b
Jason Stajich <jasonstajich.phd@gmail.com>
2023-08-13 13:52:56 -0700

    add some more extension variables in case we want to generalize

--- src/phyling/config.py
+++ src/phyling/config.py
@@ -1,3 +1,6 @@
 """Some project specific configuration parameters."""
-prot_aln_ext = 'aa.mfa'  # alignment extension
+prot_aln_ext = 'aa.mfa'  # protein alignment extension
+cds_aln_ext = 'cds.mfa'  # cds alignment extension
 protein_ext = 'faa'  # protein fasta files
+cds_ext = 'cds'  # coding sequence alignment file extension
+aln_ext = 'mfa'  # general alignment extension
