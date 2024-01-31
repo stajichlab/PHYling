60a318c92b03efd4c4b1448c22f3017914c378bd
Cheng-Hung <chenghung.tsai@email.ucr.edu>
2024-01-31 11:06:12 -0800

    Make the tree methods as a config param

--- src/phyling/config.py
+++ src/phyling/config.py
@@ -10,3 +10,5 @@ cds_aln_ext = "cds.mfa"  # cds alignment extension
 protein_ext = "faa"  # protein fasta files
 cds_ext = "cds"  # coding sequence alignment file extension
 aln_ext = "mfa"  # general alignment extension
+
+avail_tree_methods = {"upgma": "UPGMA", "nj": "Neighbor Joining", "ft": "FastTree"}
