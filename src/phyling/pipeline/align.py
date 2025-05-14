efaa94f00fb1d3737ca5b50024fde8d899fbd426
Cheng-Hung <chenghung.tsai@email.ucr.edu>
2025-05-14 12:53:47 -0700

    Fix the error estimating threads when have only 1 job.

--- src/phyling/pipeline/align.py
+++ src/phyling/pipeline/align.py
@@ -149,7 +149,7 @@ def align(
     remaining_samples, searchhits = output_precheck.precheck()
     if remaining_samples:
         logger.info("Search start...")
-        threads_per_jobs = threads if threads < 8 else 4
+        threads_per_jobs = threads if (threads < 8 or len(remaining_samples) == 1) else 4
         jobs = threads // threads_per_jobs
         hits = remaining_samples.search(hmmmarkerset, evalue=evalue, jobs=jobs, threads=threads_per_jobs)
         searchhits.update(hits)
